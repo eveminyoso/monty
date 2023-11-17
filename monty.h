5c377973c8003e6db970a80ecafa7198a76f13af
eveminyoso <eveminyoso@gmail.com>
2023-11-17 10:33:12 -0800

    monty.h

--- monty.h
+++ monty.h
@@ -37,4 +37,5 @@ typedef struct instruction_s
 void push(stack_t **stack, int value, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void execute_instruction(stack_t **stack, char *instruction, int value, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
 #endif
